9a420d95908fafadf8567fe9a036ab4a6201b533
Kenneth Rey <kennethreyt@gmail.com>
2021-11-25 11:05:46 +0800

    add bnbbusd

--- bnb.js
+++ bnb.js
@@ -1,4 +1,5 @@
 const Binance = require('node-binance-api');
+const moment = require('moment');
 const axios = require('axios');
 const colors = require('colors');
 const baseUrl = 'https://api.binance.com/api/v3/ticker/price';
@@ -6,15 +7,15 @@ const kLinesAPI = 'https://api3.binance.com/api/v3/klines?';
 
 const APISECRET = process.env.APISECRET; //API SECRET
 const APIKEY = process.env.APIKEY; //API KEY
-const SYMBOL = process.env.SYMBOL || 'SHIBBUSD'; //SYMBOL
+const SYMBOL = process.env.SYMBOL || 'BNBBUSD'; //SYMBOL
 const PERCENTCAPITAL = process.env.PERCENTCAPITAL || 100; //PERCENT CAPITAL
-const INTERVAL = process.env.INTERVAL || '1m'; //INTERVAL WHEN FETCH THE AGGREGATE TRADES
-const LIMIT = process.env.LIMIT || 10; //LIMIT WHEN FETCH THE AGGREGATE TRADES
+const INTERVAL = process.env.INTERVAL || '1h'; //INTERVAL WHEN FETCH THE AGGREGATE TRADES
+const LIMIT = process.env.LIMIT || 22; //LIMIT WHEN FETCH THE AGGREGATE TRADES
 const PERCENTBUY = process.env.PERCENTBUY; //IF NOT SET, THE AVERAGE OF THE PREVEIOS AGGREGATE TRADES
 const PERCENTSELL = process.env.PERCENTSELL; //IF NOT SET, THE AVERAGE OF THE PREVEIOS AGGREGATE TRADES
 const PRICELIMIT = process.env.PRICELIMIT; //END POINT OF THE PRICE SET
 const SPREAD = process.env.SPREAD || 1.2;
-const CRYPTO = process.env.CRYPTO || 'SHIB';
+const CRYPTO = process.env.CRYPTO || 'BNB';
 const CRYPTOTRANSACT = process.env.CRYPTO || 'BUSD';
 
 const binance = new Binance().options({
@@ -29,7 +30,9 @@ const border =
 
 //calculate the average of an array and properties
 const avg = async (arr, prop) =>
-  (await arr.reduce((r, c) => r + parseFloat(c[prop]), 0)) / arr.length;
+  (
+    (await arr.reduce((r, c) => r + parseFloat(c[prop]), 0)) / arr.length
+  ).toFixed(1);
 
 //fetch current price
 const fetchCurPrice = async () => {
@@ -37,7 +40,7 @@ const fetchCurPrice = async () => {
     params: { symbol: SYMBOL }
   });
 
-  return res.data.price;
+  return parseFloat(res.data.price);
 };
 
 //fetch the aggregate trades
@@ -64,14 +67,17 @@ const transactBuy = async () => {
   const prevHighPer = await previousHighPercent();
   const arr = await fetchAggTrades();
   const open = await avg(arr, 1);
-  const low = await avg(arr, 3);
+  const low = parseFloat(await avg(arr, 3));
   const high = await avg(arr, 2);
   const aveHigh = PERCENTSELL || (await ((high / open) * 100 - 100));
 
-  if (prevHighPer > aveHigh) {
-    console.log('AVERAGE HIGH PERCENT IS GREATER THAT PREVIOUS HIGH PERCENT');
-    return;
-  }
+  // console.log('arr', arr);
+  // console.log('low', open);
+
+  // if (prevHighPer > aveHigh) {
+  //   console.log('AVERAGE HIGH PERCENT IS GREATER THAT PREVIOUS HIGH PERCENT');
+  //   return;
+  // }
 
   //if PERCENTBBUY is not set, the average low percentage will be replace
   PERCENTBUY && console.log('PERCENT BUY PARAM EXIST : ', PERCENTBUY);
@@ -83,32 +89,36 @@ const transactBuy = async () => {
   binance.balance(async (err, bal) => {
     try {
       const currPrice = await fetchCurPrice();
-      const curBalance = bal[CRYPTOTRANSACT].available;
-      const pricetoBuy = (
-        ((100 - parseFloat(lowExec)) * currPrice) /
-        100
-      ).toFixed(8);
-      const capital = curBalance * (PERCENTCAPITAL / 100);
+      const curBalance = parseFloat(bal[CRYPTOTRANSACT].available);
 
-      console.log('currPrice', currPrice);
-      console.log('pricetoBuy', pricetoBuy);
-      console.log('aveLow', aveLow);
-      console.log('lowExec', lowExec);
-      console.log('PERCENTCAPITAL', PERCENTCAPITAL);
-      console.log('capital', capital);
+      const price = low;
+      const capital = parseFloat(
+        (curBalance * (PERCENTCAPITAL / 100)).toFixed(2)
+      );
+      const quantity = parseFloat((capital / price).toFixed(3)) - 0.001;
 
-      const amount = capital / pricetoBuy;
-      const amountRnd = Math.floor(amount);
+      console.log('Current Price :', currPrice);
+      console.log('Current Balance :', curBalance);
+      console.log(`Average Low price for ${INTERVAL.blue} :`, low);
+      console.log('Percent Capital :', PERCENTCAPITAL, '%');
+      console.log('Capital', capital);
+
+      console.log(border);
+      console.log('quantity', quantity);
+      console.log('price', price);
 
       await binance.buy(
-        SYMBOL,
-        amountRnd,
-        pricetoBuy,
+        'BNBBUSD',
+        quantity,
+        price,
         { type: 'LIMIT' },
         (err, res) => {
           try {
-            console.info(`Successfully added Buy \nPrice : ${res.price}`);
+            console.info(
+              `Successfully added Buy \nPrice : ${parseFloat(res.price)}`
+            );
           } catch (error) {
+            console.log(err);
             throw err;
           }
         }
@@ -125,32 +135,38 @@ const sell = async () => {
   const open = await avg(arr, 1);
   const high = await avg(arr, 2);
   PERCENTSELL && console.log('PERCENT SELL PARAM EXIST : ', PERCENTSELL);
-  const aveHigh = PERCENTSELL || (await ((high / open) * 100 - 100).toFixed(2));
+  const aveHigh =
+    PERCENTSELL || (await parseFloat(((high / open) * 100 - 100).toFixed(2)));
   const highExec = SPREAD * aveHigh;
   binance.balance(async (err, bal) => {
     try {
       await binance.trades(SYMBOL, (err, prevTransact) => {
         if (!err) {
-          const prevBuy = prevTransact.slice(-1)[0].price;
+          const prevBuy = parseFloat(prevTransact.slice(-1)[0].price);
           const curBalance = bal[CRYPTO].available;
-          const pricetoSell = (
-            ((100 + parseFloat(highExec)) * prevBuy) /
-            100
-          ).toFixed(8);
-          const amountRnd = Math.floor(curBalance);
-          console.log('prevBuy', prevBuy);
-          console.log('pricetoSell', pricetoSell);
-          console.log('aveHigh', aveHigh);
-          console.log('highExec', highExec);
+          const sell = (((100 + parseFloat(highExec)) * prevBuy) / 100).toFixed(
+            1
+          );
+          const quantity = parseFloat(curBalance).toFixed(3);
+          console.log('Previous Transaction :', prevBuy);
+          console.log('Price to Sell :', sell);
+          console.log(
+            `Average high in ${LIMIT} per ${INTERVAL.blue} :`,
+            aveHigh
+          );
+          console.log('Quantity :', quantity);
           binance.sell(
             SYMBOL,
-            amountRnd,
-            pricetoSell,
+            quantity,
+            sell,
             { type: 'LIMIT' },
             (err, res) => {
               try {
-                console.info(`Successfully added Sell \nPrice : ${res.price}`);
+                console.info(
+                  `Successfully added Sell \nPrice : ${parseFloat(res.price)}`
+                );
               } catch (error) {
+                console.log(err);
                 throw err;
               }
             }
@@ -170,7 +186,7 @@ const previousHighPercent = async () => {
   const res = await axios.get(kLinesAPI, {
     params: {
       symbol: SYMBOL,
-      interval: '1m',
+      interval: INTERVAL,
       limit: 1
     }
   });
