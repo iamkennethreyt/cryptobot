2936ffeab2a6c1bede58660f5a962bde8ea63a38
Kenneth Rey <kennethreyt@gmail.com>
2021-12-01 11:48:58 +0800

    added santos

--- santos.js
+++ santos.js
@@ -1,14 +1,7 @@
 const Binance = require('node-binance-api');
-// const items = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'S'];
-// for (let i = 1; i <= 60; i++) {
-//   const item = items[Math.floor(Math.random() * items.length)];
-//   setTimeout(() => {
-//     console.log(item);
-//   }, i * 1000);
-// }
 const APISECRET = process.env.APISECRET; //API SECRET
 const APIKEY = process.env.APIKEY; //API KEY
-const SYMBOL = process.env.SYMBOL || 'SANTOSBUSD'; //SYMBOL
+const SYMBOL = 'SANTOSBUSD';
 
 const binance = new Binance().options({
   APIKEY,
@@ -18,13 +11,14 @@ const binance = new Binance().options({
   verbose: true
 });
 
-const quantity = 10;
+const loop = 12000;
+const quantity = 45;
+const milisecond = 10;
 const price = 5;
 
 const buy = () => {
   binance.useServerTime();
-  for (let i = 1; i <= 1000; i++) {
-    // const item = items[Math.floor(Math.random() * items.length)];
+  for (let i = 1; i <= loop; i++) {
     setTimeout(() => {
       binance.buy(SYMBOL, quantity, price, { type: 'LIMIT' }, (err, res) => {
         if (err) {
@@ -33,13 +27,13 @@ const buy = () => {
           console.log(res);
         }
       });
-    }, i * 300);
+    }, i * milisecond);
   }
 };
 
 const sell = () => {
-  const sellQty = 40; //40 SANTOS available balance
-  const sellPrice = 10; //10 SANTOS
+  const sellQty = 45; //40 SANTOS available balance
+  const sellPrice = 20; //10 SANTOS
 
   binance.sell(SYMBOL, sellQty, sellPrice, (err, res) => {
     if (err) {
